6abae01c9f67a1c97eb98028d8b818368ba2f979
Methmi <methmiyapa@gmail.com>
2022-02-12 00:06:57 +0530

    payement

--- src/models/calculateSalary.model.js
+++ src/models/calculateSalary.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //create schema with field names
 const calculateSalarySchema = new mongoose.Schema({
-    Employeeid: { type: String, required: true, trim: true },
+    Employeeid: { type: String, required: true, trim: true, unique: true },
     Attendence: { type: Number, required: true },
     Salary: { type: Number, required: true },
     OTHourse: { type: Number, required: true },
--- src/models/insertpayment.model.js
+++ src/models/insertpayment.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //create schema with field names
 const insertpaymentSchema = new mongoose.Schema({
-    Paymentid: { type: Number, required: true },
+    Paymentid: { type: Number, required: true, unique: true, match: [/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/] },
     BillType: { type: String, required: true, trim: true },
     BillDate: { type: String, required: true, trim: true },
     BillAmount: { type: Number, required: true },
